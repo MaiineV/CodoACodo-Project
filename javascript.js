347cdd0b28e053848dbf6e6a2d1fce7c93445f2d
coti canafoglia <coticanafoglia@gmail.com>
2022-10-28 21:20:47 -0300

    formulario.js

--- javascript.js
+++ javascript.js
@@ -1,21 +1,29 @@
 function validarEnviar() {
     //valido el nombre
     if (document.fvalida.nombre.value.length <= 2) {
-        alert("Tiene que escribir su nombre")
+        alert("Debe escribir su nombre")
         document.fvalida.nombre.focus()
+        return 0;
+    }
+
+    //valido el apellido
+    if (document.fvalida.apellido.value.length <= 2) {
+        alert("Debe escribir su apellido")
+        document.fvalida.apellido.focus()
         return 0;
     }
     //valido el motivo de contacto
     if (document.fvalida.tema.selectedIndex == 0) {
         alert("Debe seleccionar un motivo de su contacto.")
-        document.fvalida.interes.focus()
+        document.fvalida.tema.focus()
         return 0;
     }
+
     //valido el comentario
     comentario = document.fvalida.comentario.value
     if (comentario == "") {
-        alert("Tiene que introducir un comentario")
-        document.fvalida.edad.focus()
+        alert("Debe escribir un comentario")
+        document.fvalida.comentario.focus()
         return 0;
     } 
     
